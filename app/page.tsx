b785515b364ed6cc1deb0f5e3574a9ec9070ef98
Jason <jason.melton2@gmail.com>
2025-05-14 01:49:22 -0500

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="bg-green-500 text-white p-8 text-center">
-      <h1 className="text-4xl font-bold">Tailwind is working!</h1>
-    </main>
-  );
+  return <main></main>;
 }
