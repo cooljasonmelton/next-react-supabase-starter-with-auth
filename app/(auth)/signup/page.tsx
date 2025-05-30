e9d1d43efb5d02698a100f8d8d1332b61b889ce5
Jason <jason.melton2@gmail.com>
2025-05-30 12:56:06 -0500

    clean up auth form, signup, login pages

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -3,7 +3,7 @@ import AuthForm from "@/components/AuthForm";
 export default function SignUpPage() {
   return (
     <div>
-      <AuthForm />
+      <AuthForm isSignUpPage />
     </div>
   );
 }
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -5,13 +5,22 @@ import { useRouter } from "next/navigation";
 
 // TODO: move text to consts
 // TODO: improve styles, submit button bg color, error doesnt jump, error color
+const LOGIN_TITLE = "Log in";
+const SIGNUP_TITLE = "Sign up";
+const EMAIL_PLACEHOLDER_TEXT = "email";
+const PASSWORD_PLACEHOLDER_TEXT = "password";
+const SUBMIT_BTN_LOADING_CTA = "Loading...";
+const SUBMIT_BTN_LOGIN_CTA = "Log in";
+const SUBMIT_BTN_SIGNUP_CTA = "Sign up";
+const SWITCH_TO_SIGNUP_TEXT = "Need an account? Sign up";
+const SWITCH_TO_LOGIN_TEXT = "Have an account? Log in";
 
-const AuthForm = () => {
+const AuthForm = ({ isSignUpPage = false }: { isSignUpPage?: boolean }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const [isSignUp, setIsSignUp] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(isSignUpPage);
 
   const router = useRouter();
   const supabase = createClient();
@@ -36,35 +45,43 @@ const AuthForm = () => {
     setLoading(false);
   };
 
+  const title = isSignUp ? SIGNUP_TITLE : LOGIN_TITLE;
+  const submitCta = isSignUp ? SUBMIT_BTN_SIGNUP_CTA : SUBMIT_BTN_LOGIN_CTA;
+  const loadingOrSubmitCta = loading ? SUBMIT_BTN_LOADING_CTA : submitCta;
+
+  const switchFormCta = isSignUp ? SWITCH_TO_LOGIN_TEXT : SWITCH_TO_SIGNUP_TEXT;
+
   return (
     <div className="p-4 max-w-sm mx-auto space-y-4">
-      <h2 className="mt-6 text-center text-3xl font-extrabold">Log in</h2>
+      <h2 className="mt-6 text-center text-3xl font-extrabold">{title}</h2>
       {error && <p className="text-[#E047DD] font-extrabold">{error}</p>}
       <input
         className="w-full border p-2"
-        placeholder="email"
+        placeholder={EMAIL_PLACEHOLDER_TEXT}
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
         className="w-full border p-2"
         type="password"
-        placeholder="password"
+        placeholder={PASSWORD_PLACEHOLDER_TEXT}
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
       <button
-        className="bg-black text-white px-4 py-2"
+        className="bg-black text-white px-4 py-2 cursor-pointer"
         onClick={handleSignIn}
         disabled={loading}
       >
-        {loading ? "Loading..." : isSignUp ? "Sign Up" : "Log In"}
+        {loadingOrSubmitCta}
       </button>
+
+      {/* TODO: make link or button, check a11y */}
       <p
         className="text-sm underline cursor-pointer"
         onClick={() => setIsSignUp(!isSignUp)}
       >
-        {isSignUp ? "Already have an account? Log in" : "No account? Sign up"}
+        {switchFormCta}
       </p>
     </div>
   );
