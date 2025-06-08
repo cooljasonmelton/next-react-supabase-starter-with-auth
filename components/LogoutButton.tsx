f28374e0bc731c2d7fde804bdd03f5354f8aa41c
Jason <jason.melton2@gmail.com>
2025-06-08 03:01:57 -0500

    cleanup nav and adjust variables

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,55 +1,22 @@
 "use client";
 
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { Modal } from "./Modal";
-
-// TODO: figure out best style and placement of logout button
 
 const BUTTON_CTA = "Log Out";
-const MODAL_TITLE = "ARE YOU SURE?";
-const MODAL_CANCEL_CTA = "Cancel";
 
 export const LogoutButton = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   const supabase = createClientComponentClient();
   const router = useRouter();
 
   const handleLogout = async () => {
-    setIsModalOpen(false);
     await supabase.auth.signOut();
     router.push("/");
   };
 
   return (
-    <div className="">
-      <button
-        className="px-4 py-0.5 bg-red-500 text-white rounded hover:bg-red-600"
-        onClick={() => setIsModalOpen(true)}
-      >
-        {BUTTON_CTA}
-      </button>
-      {/* confirmation modal */}
-      {isModalOpen && (
-        <Modal title={MODAL_TITLE} onClose={() => setIsModalOpen(false)}>
-          <div className="flex gap-3 justify-center align-center">
-            <button
-              className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              onClick={() => setIsModalOpen(false)}
-            >
-              {MODAL_CANCEL_CTA}
-            </button>
-            <button
-              className="rounded-md bg-red-500 px-3 py-2 text-sm text-white font-semibold shadow-sm ring-1 ring-inset ring-red-600 rounded hover:bg-red-600"
-              onClick={handleLogout}
-            >
-              {BUTTON_CTA}
-            </button>
-          </div>
-        </Modal>
-      )}
-    </div>
+    <button className="btn-primary-sm btn-primary:hover" onClick={handleLogout}>
+      {BUTTON_CTA}
+    </button>
   );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,23 +5,13 @@ import { LogoutButton } from "@/components/LogoutButton";
 import { useAuth } from "@/app/hooks/useAuth";
 
 const NAVBAR_TITLE = "NEW PROJECT";
-const DASHBOARD_LINK_TEXT = "Dashboard";
 const LOGIN_LINK_TEXT = "Login";
-
-// const navLinkMap = {
-//   dashboard: {
-//     href: "/dashboard",
-//     text: DASHBOARD_LINK_TEXT,
-//   },
-//   login: {
-//     href: "/login",
-//     text: LOGIN_LINK_TEXT,
-//   },
-// };
 
 const SignedInNavLinks = () => (
   <>
-    <Link href="/dashboard">{DASHBOARD_LINK_TEXT}</Link>
+    {/* explore */}
+    {/* contact */}
+    {/* etc. */}
   </>
 );
 
@@ -35,11 +25,11 @@ const Navbar = () => {
   const { loading, isSignedIn } = useAuth();
 
   return (
-    <nav className="w-full px-4 py-2 border-b flex justify-between items-center">
+    <nav className="w-full border-b flex justify-between items-center p-2">
       <div className="text-xl font-bold">
         <Link href="/">{NAVBAR_TITLE}</Link>
       </div>
-      <div className="space-x-8 flex flex-nowrap">
+      <div className="space-x-8 space-y-8 flex flex-nowrap font-bold">
         {!loading && (
           <>
             {isSignedIn ? <SignedInNavLinks /> : <SignedOutNavLinks />}
